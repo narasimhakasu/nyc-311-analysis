d1eb3619e9a476de31d4a0e7e7d3367ae2cd2ad0
narasimhakasu <narasimha.kasu9@gmail.com>
2026-03-12 20:05:01 +0530

    Rename table for data ingestion

--- queries/01_data_ingestion.sql
+++ queries/01_data_ingestion.sql
@@ -1,4 +1,4 @@
-create or replace table `nyc-311-service-requests.nyc_311.table_raw` as
+create or replace table `nyc-311-servicerequests.staging.raw_data` as
 select *
 from `bigquery-public-data.new_york_311.311_service_requests`
 where created_date between '2021-01-01' AND '2021-11-8'
